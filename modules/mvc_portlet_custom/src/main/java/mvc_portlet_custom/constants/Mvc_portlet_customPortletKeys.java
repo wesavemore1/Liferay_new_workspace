17442a712e08656c8d4ea915272cd0c582bb3097
srikanthvelugoti <srikanth.velugoti@outlook.com>
2021-05-30 21:40:16 +0530

    added a portlet

---
+++ modules/mvc_portlet_custom/src/main/java/mvc_portlet_custom/constants/Mvc_portlet_customPortletKeys.java
@@ -0,0 +1,11 @@
+package mvc_portlet_custom.constants;
+
+/**
+ * @author RSTPL065
+ */
+public class Mvc_portlet_customPortletKeys {
+
+	public static final String MVC_PORTLET_CUSTOM =
+		"mvc_portlet_custom_Mvc_portlet_customPortlet";
+
+}
---
+++ modules/mvc_portlet_custom/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="mvc_portlet_custom.caption"/></b>
+</p>
